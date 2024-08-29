a0f472872c3c6b22169bb998d986df4f02b56063
Taisiya Nesterenko <prostotaya@gmail.com>
2024-08-30 00:04:54 +0300

    Добавлена новая ветка с выполнением арифметических операций

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: " );
+        int number2 =  new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел: "+(number1+number2));
+        System.out.println("Разность чисел: "+(number1-number2));
+        System.out.println("Произведение: "+(number1*number2));
+        System.out.println("Частное: "+(double)number1/number2);
     }
 }
